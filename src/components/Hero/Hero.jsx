d4a8e74b2ee3560ce6a64aedd0f86362d641cd3f
ebnybuniad <ebnybuniad@hmail.com>
2025-04-11 01:50:35 +0600

    added hero banner

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import bannerImage from '../../assets/banner-main.png'
+const Hero = () => {
+    const bannerImgClasses = 'mx-auto pt-[5%] pb-10'
+    const sectionClasses = 'container mx-auto rounded-2xl mt-5 text-white text-center space-y-3'
+    return (
+        <>
+            <div className={sectionClasses} id='hero-background'>
+                <img className={bannerImgClasses} src={bannerImage} alt="" />
+                <h2 className='text-4xl font-semibold'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+                <p className='text-2xl text-gray-400'>Beyond Boundaries Beyond Limits</p>
+                <button className='credit-button'><span className='border-4 px-5 py-3 rounded-xl'>Claim Free Credit</span></button>
+            </div>
+        </>
+    );
+};
+
+export default Hero;
